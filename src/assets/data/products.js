ba3ec6c5e3e4fbdff721d8623554448bc472b16d
dLuxKid <adetunjimarvellous09@gmail.com>
2024-01-05 14:22:43 +0100

    code:done with landing page

---
+++ src/assets/data/products.js
@@ -0,0 +1,17 @@
+import pods from "../Pod_update.webp";
+import trousers from "../trousers.webp";
+import longShirt from "../shirt-long.webp";
+import tShirt from "../shirt-t.webp";
+import shorts from "../shorts.webp";
+import Vinyl from "../Vinyl.webp";
+
+const products = [
+  { id: 0, img: pods, price: "#193,300.00", name: "YZY PODS" },
+  { id: 1, img: trousers, price: "#116,000.00", name: "VULTURE PANTS" },
+  { id: 2, img: longShirt, price: "#96,700.00", name: "LONG T" },
+  { id: 3, img: shorts, price: "#96,700.00", name: "VULTURE SHORTS" },
+  { id: 4, img: tShirt, price: "#77,300.00", name: "BOX T" },
+  { id: 5, img: Vinyl },
+];
+
+export default products;
